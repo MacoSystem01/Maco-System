64d2323d19cd7b8a616760fcec5f13c925b7e53d
MACO System <92897308+MacoSystem01@users.noreply.github.com>
2023-12-11 13:44:47 -0500

    Add files via upload

---
+++ com/server.php
@@ -0,0 +1,10 @@
+<?php
+$xmlstr = <<<XML
+<?xml version='1.0' standalone='yes'?>
+<servidor>
+ <server>
+  <ip>192.168.10.14</ip>  <!-- SE CAMBIO LA DIRECCIÓN IP -->
+ </server>
+</servidor>
+XML;
+?>
